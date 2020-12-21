27fe5c59b88e787ac03013956ce1294e63e44ff2
Hayato Ise <hayatoise.com@gmail.com>
2020-12-21 13:38:16 +0900

    feat: View the top 10 articles on Hacker News

---
+++ components/Article.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <article>
+      <a :href="article.url">
+        <h2>{{ article.title }}</h2>
+        <hr />
+      </a>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,44 @@
 <template>
-  <h1 class="website-title">Hacker News</h1>
+  <div>
+    <h1 class="website-title">Hacker News</h1>
+    <div v-for="article in articles" :key="article.id">
+      <Article :article="article" />
+    </div>
+  </div>
 </template>
+
+<script>
+import Article from '../components/Article'
+
+export default {
+  components: {
+    Article,
+  },
+  async asyncData({ $axios }) {
+    const topStoryIds = await $axios.$get(
+      'https://hacker-news.firebaseio.com/v0/topstories.json'
+    )
+
+    const topTenIds = topStoryIds.filter(
+      (topStoryId) => topStoryIds.indexOf(topStoryId) <= 10
+    )
+
+    const articles = []
+    let articleId
+    for (articleId in topTenIds) {
+      const itemUrl = `https://hacker-news.firebaseio.com/v0/item/${articleId}.json?print=pretty`
+      const item = await $axios.$get(itemUrl)
+      if (item) {
+        articles.push(item)
+      }
+    }
+
+    return {
+      articles,
+    }
+  },
+}
+</script>
 
 <style scoped>
 .website-title {
